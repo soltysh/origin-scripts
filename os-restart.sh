bb7cd2ce89337b8bc5c0df1e0386bd72ef9ef02e
Maciej Szulik <maszulik@redhat.com>
2015-09-24 14:03:43 +0200

    Added some sleep after restarting openshift

--- os-restart.sh
+++ os-restart.sh
@@ -7,3 +7,4 @@ echo "Starting OpenShift..."
 sudo /data/src/github.com/openshift/origin/_output/local/go/bin/openshift start \
   --master-config=$HOME/openshift.local.config/master/master-config.yaml \
   --node-config=$HOME/openshift.local.config/node-openshiftdev.local/node-config.yaml &> $HOME/logs/openshift.log &
+sleep 5
