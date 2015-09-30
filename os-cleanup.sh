75716b3c64b29b1df4528520aaee7a256f7b6c47
Maciej Szulik <maszulik@redhat.com>
2015-09-30 09:21:13 +0200

    Added prunning old images from openshift internal registry

--- os-cleanup.sh
+++ os-cleanup.sh
@@ -6,6 +6,7 @@ sudo pkill openshift
 echo "Pruning docker..."
 docker rmi $(docker images -q -f "dangling=true")
 docker rm -f $(docker ps -qa)
+docker rmi $(docker images --no-trunc | grep 172.30 | awk '{print $3}')
 
 echo "Umounting dirs..."
 mount | grep openshift.local.volumes | cut -f 3 -d " " | xargs sudo umount
