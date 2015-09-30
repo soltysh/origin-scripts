da9ed86603b60446aa45fbffe1e8e75e927ef947
Maciej Szulik <maszulik@redhat.com>
2015-09-30 09:22:41 +0200

    Changed images prunning order

--- os-cleanup.sh
+++ os-cleanup.sh
@@ -4,8 +4,8 @@ echo "Killing OpenShift..."
 sudo pkill openshift
 
 echo "Pruning docker..."
-docker rmi $(docker images -q -f "dangling=true")
 docker rm -f $(docker ps -qa)
+docker rmi $(docker images -q -f "dangling=true")
 docker rmi $(docker images --no-trunc | grep 172.30 | awk '{print $3}')
 
 echo "Umounting dirs..."
