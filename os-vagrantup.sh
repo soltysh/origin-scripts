3ea5d257815007d95e06702c9ccab7598672d846
Maciej Szulik <maszulik@redhat.com>
2015-10-28 09:37:16 +0100

    Added script for setting up aws environment

---
+++ os-vagrantup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd $GOPATH/src/github.com/openshift/origin
+
+echo "Starting openshiftdev..."
+vagrant origin-init --stage inst --os rhel7 --instance-type c4.large maszulik-dev
+vagrant up --provider aws
+vagrant sync-origin -s
+
+guest_ip=$(vagrant ssh-config | grep HostName | awk '{ print $2 }')
+guest_ip="ec2-${guest_ip//./-}.compute-1.amazonaws.com"
+
+scp $GOPATH/src/github.com/soltysh/origin-scripts/* $guest_ip:
+exec ssh $guest_ip
