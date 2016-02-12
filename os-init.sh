dd8f75392ff7b16fd323931ab7631a9e4719a690
Maciej Szulik <maszulik@redhat.com>
2016-02-12 13:37:12 +0100

    Added small timeout for when it takes a bit longer for docker-registry to be ready

--- os-init.sh
+++ os-init.sh
@@ -67,11 +67,20 @@ oc login localhost:8443 \
     --certificate-authority=$HOME/openshift.local.config/master/ca.crt
 
 echo "Logging in to docker registry..."
-token=$(oc whoami -t)
 registry=$(sudo /data/src/github.com/openshift/origin/_output/local/bin/linux/amd64/oc \
     --config=$HOME/openshift.local.config/master/admin.kubeconfig \
     --template='{{.spec.portalIP}}:{{(index .spec.ports 0).port}}' \
     get svc/docker-registry)
+set +e
+while true; do
+    curl --max-time 2 -fs http://${registry}/healthz
+    if [ $? -eq 0 ]; then
+        break
+    fi
+    sleep 1
+done
+set -e
+token=$(oc whoami -t)
 docker login -u test-admin -e test@example.org -p ${token} ${registry}
 
 echo "Creating new project..."
