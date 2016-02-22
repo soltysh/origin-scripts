e14492a66a4a427ef8530248387297b9b1d6a986
Maciej Szulik <maszulik@redhat.com>
2016-02-22 13:58:39 +0100

    Conditionally log in into the registry

--- os-init.sh
+++ os-init.sh
@@ -66,22 +66,25 @@ oc login localhost:8443 \
     -u test-admin -p pass \
     --certificate-authority=$HOME/openshift.local.config/master/ca.crt
 
-echo "Logging in to docker registry..."
-registry=$(sudo /data/src/github.com/openshift/origin/_output/local/bin/linux/amd64/oc \
-    --config=$HOME/openshift.local.config/master/admin.kubeconfig \
-    --template='{{.spec.portalIP}}:{{(index .spec.ports 0).port}}' \
-    get svc/docker-registry)
-set +e
-while true; do
-    curl --max-time 2 -fs http://${registry}/healthz
-    if [ $? -eq 0 ]; then
-        break
-    fi
-    sleep 1
-done
-set -e
-token=$(oc whoami -t)
-docker login -u test-admin -e test@example.org -p ${token} ${registry}
+DOCKER_LOGIN=${DOCKER_LOGIN:-""}
+if [ -n "${DOCKER_LOGIN}" ]; then
+    echo "Logging in to docker registry..."
+    registry=$(sudo /data/src/github.com/openshift/origin/_output/local/bin/linux/amd64/oc \
+        --config=$HOME/openshift.local.config/master/admin.kubeconfig \
+        --template='{{.spec.portalIP}}:{{(index .spec.ports 0).port}}' \
+        get svc/docker-registry)
+    set +e
+    while true; do
+        curl --max-time 2 -fs http://${registry}/healthz
+        if [ $? -eq 0 ]; then
+            break
+        fi
+        sleep 1
+    done
+    set -e
+    token=$(oc whoami -t)
+    docker login -u test-admin -e test@example.org -p ${token} ${registry}
+fi
 
 echo "Creating new project..."
 oc new-project test \
