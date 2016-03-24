dfb6764d3ffc73d45218a778696120417d0f9a13
Maciej Szulik <maszulik@redhat.com>
2016-03-24 09:37:27 +0100

    Moved docker login to its own script

---
+++ os-dockerlogin.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+echo "Logging in to docker registry..."
+registry=$(sudo /data/src/github.com/openshift/origin/_output/local/bin/linux/amd64/oc \
+    --config=$HOME/openshift.local.config/master/admin.kubeconfig \
+    --template='{{.spec.portalIP}}:{{(index .spec.ports 0).port}}' \
+    get svc/docker-registry)
+set +e
+while true; do
+    curl --max-time 2 -fs http://${registry}/healthz
+    if [ $? -eq 0 ]; then
+        break
+    fi
+    sleep 1
+done
+set -e
+token=$(oc whoami -t)
+docker login -u test-admin -e test@example.org -p ${token} ${registry}
