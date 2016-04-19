d3b6688a921f68a576d292b7d3ef551ac128510f
Maciej Szulik <soltysh@gmail.com>
2016-04-19 14:22:00 +0200

    Fallback to default instance type for aws

--- os-vagrantup.sh
+++ os-vagrantup.sh
@@ -3,7 +3,7 @@
 cd $GOPATH/src/github.com/openshift/origin
 
 echo "[INFO] Starting openshiftdev..."
-vagrant origin-init --stage inst --os rhel7 --instance-type c4.large maszulik-dev
+vagrant origin-init --stage inst --os rhel7 maszulik-dev
 vagrant up --provider aws
 vagrant sync-origin -s
 
