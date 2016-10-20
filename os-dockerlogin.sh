3f9114d2e77dfd11f2db575a710e37ebf57acb0e
Maciej Szulik <soltysh@gmail.com>
2016-10-20 13:59:19 +0200

    Updated docker login script

--- os-dockerlogin.sh
+++ os-dockerlogin.sh
@@ -3,7 +3,7 @@
 echo "[INFO] Logging in to docker registry..."
 registry=$(sudo /data/src/github.com/openshift/origin/_output/local/bin/linux/amd64/oc \
     --config=$HOME/openshift.local.config/master/admin.kubeconfig \
-    --template='{{.spec.portalIP}}:{{(index .spec.ports 0).port}}' \
+    --template='{{.spec.clusterIP}}:{{(index .spec.ports 0).port}}' \
     get svc/docker-registry)
 set +e
 while true; do
