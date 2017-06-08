12ed57b4af6dac28570fef94377e1bbeeca021c5
Maciej Szulik <maszulik@redhat.com>
2017-06-08 22:09:20 +0200

    Tweak restart script to reflact recent dev hostname change

--- os-restart.sh
+++ os-restart.sh
@@ -7,6 +7,6 @@ echo "[INFO] Starting OpenShift..."
 loglevel=${1:-0}
 sudo /data/src/github.com/openshift/origin/_output/local/bin/linux/amd64/openshift start \
   --master-config=$HOME/openshift.local.config/master/master-config.yaml \
-  --node-config=$HOME/openshift.local.config/node-openshiftdev.local/node-config.yaml \
+  --node-config=$HOME/openshift.local.config/node-dev/node-config.yaml \
   --loglevel=$loglevel &>> $HOME/logs/openshift.log &
 sleep 5
