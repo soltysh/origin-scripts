8b5026de63e2795bc34efd228d964fd8ed86353c
Maciej Szulik <maszulik@redhat.com>
2018-10-30 11:46:17 +0100

    Update os-envup.sh

--- os-envup.sh
+++ os-envup.sh
@@ -15,21 +15,17 @@ images="docker.io/openshift/hello-openshift \
 
 echo "[INFO] Starting NFS..."
 sudo systemctl start nfs-server.service
-sudo firewall-cmd --add-service nfs
 
 echo "[INFO] Starting openshiftdev..."
 virsh start openshiftdev
 sleep 10
 
-while [[ -z "$guest_ip" ]]; do
-    guest_ip=$(arp -an | grep 52:54:00:f2:5e:34 | cut -f 2 -d "(" | cut -f 1 -d ")")
-    sleep 1
-done
+guest_ip="192.168.122.12"
 
 echo "[INFO] Checking if $guest_ip is up..."
 
 while true; do
-    ssh -t -q vagrant@$guest_ip "echo 2>&1"
+    ssh -t -q x@$guest_ip "echo 2>&1"
     if [[ $? -eq 0 ]]; then
         break
     fi
@@ -40,10 +36,10 @@ script_path=$(mktemp)
 
 cat <<EOF > $script_path
 echo "[INFO] Mounting origin..."
-sudo mount 192.168.121.1:/nfsshare/origin /data/src/github.com/openshift/origin/
+sudo mount 192.168.122.1:/nfsshare/origin /data/src/github.com/openshift/origin/
 
 echo "[INFO] Mounting k8s..."
-sudo mount 192.168.121.1:/nfsshare/kubernetes /data/src/k8s.io/kubernetes/
+sudo mount 192.168.122.1:/nfsshare/kubernetes /data/src/k8s.io/kubernetes/
 EOF
 
 # sometimes I don't want to have the images to be pulled
@@ -77,8 +73,8 @@ EOF
 fi
 
 shopt -s extglob
-scp $GOPATH/src/github.com/soltysh/origin-scripts/!(os-envup).sh vagrant@$guest_ip:bin/
-cat $script_path | ssh -t vagrant@$guest_ip
+scp $GOPATH/src/github.com/soltysh/origin-scripts/!(os-envup).sh x@$guest_ip:bin/
+cat $script_path | ssh -t x@$guest_ip
 rm -f ${script_path}
 
-exec ssh vagrant@$guest_ip
+exec ssh x@$guest_ip
